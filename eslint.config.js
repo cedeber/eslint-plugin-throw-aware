50165730286eea27281edead20fa461744359cd1
Cédric Eberhardt <hello+code@cedeber.fr>
2024-09-14 12:25:50 +0200

    add unicorn eslint plugin

--- eslint.config.js
+++ eslint.config.js
@@ -3,17 +3,20 @@ import pluginJs from "@eslint/js";
 import eslintPlugin from "eslint-plugin-eslint-plugin";
 import nodePlugin from "eslint-plugin-n";
 import avaPlugin from "eslint-plugin-ava";
+import eslintPluginUnicorn from "eslint-plugin-unicorn";
 
 /** @type import("eslint").Linter.Config[] */
 export default [
   { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
   eslintPlugin.configs["flat/recommended"],
+  eslintPluginUnicorn.configs["flat/recommended"],
   nodePlugin.configs["flat/recommended-script"],
   {
     rules: {
       "eslint-plugin/require-meta-docs-description": "error",
       "n/exports-style": ["error", "module.exports"],
+      "unicorn/prevent-abbreviations": "off",
     },
   },
   { languageOptions: { ecmaVersion: 2020, sourceType: "module" } },
